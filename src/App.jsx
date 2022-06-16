068eede4a0ea949d0e7ac7a7461c275d5e611997
Koshal Raj Shakya <koshalshakya72@gmail.com>
2022-06-16 17:54:13 +0545

    Initial Components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => <div>App</div>;
+import ExcerciseDetail from './pages/ExcerciseDetail';
+import Home from './pages/Home';
+import NavBar from './components/Navbar';
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <Box width="400px">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/excercise/:id" element={<ExcerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
 export default App;
